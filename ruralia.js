f57a3ebee9fcd30389c2b4b649af71177c89ecd3
cadavincito <cadavincinfo@gmail.com>
2023-06-07 22:53:12 -0500

    modal hecho

---
+++ ruralia.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.getElementById('myImage');
+    image.addEventListener('click', function() {
+      // Your code here
+      console.log('Image clickiada!');
+    });
+  });
+  
